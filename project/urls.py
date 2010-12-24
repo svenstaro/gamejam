7dca87d7145d2b7803993dd5269b09ca65505025
Matz Radloff <matzradloff@gmx.de>
2010-12-24 04:51:17 +0100

    URLs for admin docs.

--- project/urls.py
+++ project/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     (r'^$', include('gamejam.jam.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
