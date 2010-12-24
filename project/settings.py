353f529471702a548c513d01aa3a90bc2708eaf8
Matz Radloff <matzradloff@gmx.de>
2010-12-24 05:28:28 +0100

    Now using apache and serving static files now correctly.

--- project/settings.py
+++ project/settings.py
@@ -52,7 +52,7 @@ MEDIA_ROOT = '/srv/django/gamejam/static/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://lord.game-server.cc:8000/static/'
+MEDIA_URL = 'http://lord.game-server.cc/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
