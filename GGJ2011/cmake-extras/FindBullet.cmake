44cc2fb017da369225a79c12e462a0be21e81fa9
Sven-Hendrik Haase <sh@lutzhaase.com>
2011-02-06 12:04:17 +0100

    Rewriting FindBullet.cmake to make it easier to cross compile.

--- GGJ2011/cmake-extras/FindBullet.cmake
+++ GGJ2011/cmake-extras/FindBullet.cmake
@@ -7,15 +7,9 @@
 #  BULLET_LIBRARIES - Link to this, by default it includes
 #                     all bullet components (Dynamics,
 #                     Collision, LinearMath, & SoftBody)
-#
-#  This module accepts the following variables
-#
-#  BULLET_ROOT - Can be set to bullet install path or Windows build path
-#
 
 #=============================================================================
-# Copyright 2009 Kitware, Inc.
-# Copyright 2009 Philip Lowman <philip@yhbt.com>
+# Copyright 2011 Sven-Hendrik Haase <sh@lutzhaase.com>
 #
 # Distributed under the OSI-approved BSD License (the "License");
 # see accompanying file Copyright.txt for details.
@@ -27,59 +21,36 @@
 # (To distributed this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
-macro(_FIND_BULLET_LIBRARY _var)
-  find_library(${_var}
-     NAMES 
-        ${ARGN}
-     PATHS
-        ${BULLET_ROOT}
-        ${BULLET_ROOT}/out/release8/libs
-        ${BULLET_ROOT}/out/debug8/libs
-     PATH_SUFFIXES lib
-  )
-  mark_as_advanced(${_var})
-endmacro()
+FIND_PATH(BULLET_INCLUDE_DIR btBulletCollisionCommon.h
+          PATH_SUFFIXES bullet)
 
-macro(_BULLET_APPEND_LIBRARIES _list _release)
-   set(_debug ${_release}_DEBUG)
-   if(${_debug})
-      set(${_list} ${${_list}} optimized ${${_release}} debug ${${_debug}})
-   else()
-      set(${_list} ${${_list}} ${${_release}})
-   endif()
-endmacro()
+FIND_LIBRARY(BULLET_COLLISION_LIBRARY NAMES BulletCollision )
+FIND_LIBRARY(BULLET_DYNAMICS_LIBRARY NAMES BulletDynamics )
+FIND_LIBRARY(BULLET_MULTITHREADED_LIBRARY NAMES BulletMultiThreaded )
+FIND_LIBRARY(BULLET_SOFTBODY_LIBRARY NAMES BulletSoftBody )
+FIND_LIBRARY(BULLET_SOFTBODYSOLVERS_CPU_LIBRARY NAMES BulletSoftBodySolvers_CPU )
+FIND_LIBRARY(BULLET_CONVEXDECOMPOSITION_LIBRARY NAMES ConvexDecomposition )
+FIND_LIBRARY(BULLET_GIMPACTUTILS_LIBRARY NAMES GIMPACTUtils )
+FIND_LIBRARY(BULLET_LINEARMATH_LIBRARY NAMES LinearMath )
+FIND_LIBRARY(BULLET_MINICL_LIBRARY NAMES MiniCL )
+SET(BULLET_LIBRARIES ${BULLET_COLLISION_LIBRARY}
+                   ${BULLET_DYNAMICS_LIBRARY}
+                   ${BULLET_MULTITHREADED_LIBRARY}
+                   ${BULLET_SOFTBODY_LIBRARY}
+                   ${BULLET_SOFTBODYSOLVERS_CPU_LIBRARY}
+                   ${BULLET_CONVEXDECOMPOSITION_LIBRARY}
+                   ${BULLET_GIMPACTUTILS_LIBRARY}
+                   ${BULLET_LINEARMATH_LIBRARY}
+                   ${BULLET_MINICL_LIBRARY})
 
-find_path(BULLET_INCLUDE_DIR NAMES btBulletCollisionCommon.h
-  PATHS
-    /usr/include/bullet
-    /usr/local/include/bullet
-    ${BULLET_ROOT}/include
-    ${BULLET_ROOT}/src
-)
-
-# Find the libraries
-
-_FIND_BULLET_LIBRARY(BULLET_DYNAMICS_LIBRARY        BulletDynamics)
-_FIND_BULLET_LIBRARY(BULLET_DYNAMICS_LIBRARY_DEBUG  BulletDynamics_d)
-_FIND_BULLET_LIBRARY(BULLET_COLLISION_LIBRARY       BulletCollision)
-_FIND_BULLET_LIBRARY(BULLET_COLLISION_LIBRARY_DEBUG BulletCollision_d)
-_FIND_BULLET_LIBRARY(BULLET_MATH_LIBRARY            LinearMath BulletMath)
-_FIND_BULLET_LIBRARY(BULLET_MATH_LIBRARY_DEBUG      LinearMath_d BulletMath_d)
-_FIND_BULLET_LIBRARY(BULLET_SOFTBODY_LIBRARY        BulletSoftBody)
-_FIND_BULLET_LIBRARY(BULLET_SOFTBODY_LIBRARY_DEBUG  BulletSoftBody_d)
-
-
-# handle the QUIETLY and REQUIRED arguments and set BULLET_FOUND to TRUE if 
+# handle the QUIETLY and REQUIRED arguments and set Bullet_FOUND to TRUE if
 # all listed variables are TRUE
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Bullet DEFAULT_MSG
-    BULLET_DYNAMICS_LIBRARY BULLET_COLLISION_LIBRARY BULLET_MATH_LIBRARY
-    BULLET_SOFTBODY_LIBRARY BULLET_INCLUDE_DIR)
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Bullet DEFAULT_MSG BULLET_COLLISION_LIBRARY BULLET_INCLUDE_DIR)
 
-set(BULLET_INCLUDE_DIRS ${BULLET_INCLUDE_DIR})
-if(BULLET_FOUND)
-   _BULLET_APPEND_LIBRARIES(BULLET_LIBRARIES BULLET_DYNAMICS_LIBRARY)
-   _BULLET_APPEND_LIBRARIES(BULLET_LIBRARIES BULLET_COLLISION_LIBRARY)
-   _BULLET_APPEND_LIBRARIES(BULLET_LIBRARIES BULLET_MATH_LIBRARY)
-   _BULLET_APPEND_LIBRARIES(BULLET_LIBRARIES BULLET_SOFTBODY_LIBRARY)
-endif()
+IF (BULLET_FOUND)
+    SET(BULLET_INCLUDE_DIRS ${BULLET_INCLUDE_DIR})
+ENDIF (BULLET_FOUND)
+
+MARK_AS_ADVANCED(BULLET_INCLUDE_DIR BULLET_LIBRARIES)
+
