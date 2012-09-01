f7d325d5280829cb83cb73694a39719e20363646
Sascha Graeff <sascha@blackicegames.de>
2012-09-01 13:13:50 +0200

    Set encoding in html to prevent errors thrown in browser.

--- BGJ03/js/main.js
+++ BGJ03/js/main.js
@@ -28,6 +28,9 @@ MainState = gamvas.State.extend({
 
     draw: function(t) {
         //gamvas.physics.drawDebug();
+        //this.c.fillStyle = '#fff';
+        //this.c.font = 'bold 20px sans-serif';
+		//this.c.fillText("This level is hard as hell.", 0, 0);
     },
 
     onMouseDown: function(b, x, y) {
