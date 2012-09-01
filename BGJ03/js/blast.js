3139f36b4e8bed754e27877e73aaa9f37717aede
Paul Bienkowski <opatutlol@aol.com>
2012-09-01 18:36:19 +0200

    Implemented blast dying and actor states

--- BGJ03/js/blast.js
+++ BGJ03/js/blast.js
@@ -1,4 +1,45 @@
-// create our collision objects
+BlastEmissionState = gamvas.ActorState.extend({
+    create: function() {
+        this._super("emission");
+        this.accumulator = 0;
+    },
+
+    update: function(t) {
+        this.accumulator += t;
+        var dt = 1 / WINDS_PER_SECOND;
+        while(this.accumulator > dt && this.actor.winds < WINDS_PER_BLAST) {
+            // spawn a wind particle
+            var w = new Wind(nextId("wind"), this.actor.position.x, this.actor.position.y, this.actor.rotation);
+            gamvas.state.getCurrentState().addActor(w);
+            this.actor.winds++;
+            this.accumulator -= dt;
+        }
+
+        if(this.actor.winds >= WINDS_PER_BLAST) {
+            this.actor.setState("die");
+        }
+    }
+});
+
+DyingState = gamvas.ActorState.extend({
+    create: function(dieTime) {
+        this._super("die");
+        this.dieTime = dieTime || 2;
+        this.timeLeft = this.dieTime;
+    },
+
+    enter: function() {
+        this.actor.dyingProgress = 0;
+    },
+
+    update: function(t) {
+        this.actor.dyingProgress = 1 - this.timeLeft / this.dieTime;
+        this.timeLeft -= t;
+        if(this.timeLeft < 0)
+            gamvas.state.getCurrentState().removeActor(this.actor);
+    }
+});
+
 Blast = gamvas.Actor.extend({
     create: function(name, tile, normal) {
         this._super(name, 
@@ -13,22 +54,10 @@ Blast = gamvas.Actor.extend({
         this.setAnimation("idle");
         this.center = new gamvas.Vector2D(0, 16);
 
-        this.accumulator = 0;
         this.winds = 0;
-
-        this.getCurrentState().update = function(t) {
-            this.actor.accumulator += t;
-            var dt = 1 / WINDS_PER_SECOND;
-            while(this.actor.accumulator > dt && this.actor.winds < WINDS_PER_BLAST) {
-                // spawn a wind particle
-                var w = new Wind(nextId("wind"), this.actor.position.x, this.actor.position.y, this.actor.rotation);
-                gamvas.state.getCurrentState().addActor(w);
-                this.actor.winds++;
-                this.actor.accumulator -= dt;
-            }
-
-
-        };
+        this.addState(new BlastEmissionState());
+        this.addState(new DyingState(1));
+        this.setState("emission");
     }
 });
 
