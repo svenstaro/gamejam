e12fe295a95d1de96ba9451249cd5003909dbe82
Sascha Graeff <sascha@blackicegames.de>
2012-09-01 15:01:52 +0200

    Added gears, created tileset, added test level.

--- BGJ03/js/tile.js
+++ BGJ03/js/tile.js
@@ -6,7 +6,7 @@ Tile = gamvas.Actor.extend({
 
         var st = gamvas.state.getCurrentState();
 
-		this.image = new gamvas.Image(st.resource.getImage('gfx/wall9.png'));
+		this.image = new gamvas.Image(st.resource.getImage('gfx/tileset.png'));
 		this.image.position = new gamvas.Vector2D(this.position.x, this.position.y);
 		this.image.move(-(xOffset + 0.5) * TILESIZE, -(yOffset + 0.5) * TILESIZE);
 		this.image.setClipRect(xOffset * TILESIZE, yOffset * TILESIZE, TILESIZE, TILESIZE);
