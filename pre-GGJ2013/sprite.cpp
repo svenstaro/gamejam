ee137d68af3ac0be5a55d603652f85346e27b3dc
Sven-Hendrik Haase <sh@lutzhaase.com>
2013-01-21 03:49:27 +0100

    Add scaling properties

--- pre-GGJ2013/sprite.cpp
+++ pre-GGJ2013/sprite.cpp
@@ -1,8 +1,11 @@
+#include <QPointF>
+
 #include "sprite.hpp"
 
 Sprite::Sprite(Game *game, QString file) :
     GameObject(game),
-    m_pixmap(file)
+    m_pixmap(file),
+    m_scale(QPointF(1.f, 1.f))
 {
 }
 
@@ -13,10 +16,26 @@ void Sprite::prePaint(QPainter *painter)
 void Sprite::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
     prePaint(painter);
-    painter->drawPixmap(-m_pixmap.width() / 2, -m_pixmap.height() / 2, m_pixmap);
+    painter->drawPixmap((-m_pixmap.width() / 2) * m_scale.x(),
+                        (-m_pixmap.height() / 2) * m_scale.y(),
+                        m_pixmap);
+    Q_UNUSED(painter);
+    Q_UNUSED(option);
+    Q_UNUSED(widget);
 }
 
 QRectF Sprite::boundingRect() const
 {
     return m_pixmap.rect();
+}
+
+QPointF Sprite::scale() const
+{
+    return m_scale;
+}
+
+void Sprite::setScale(QPointF scale)
+{
+    m_scale = scale;
+    qDebug() << m_scale;
 }
