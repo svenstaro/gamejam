40155db51157bd86b345d685f574c506a266c778
Paul Bienkowski <opatutlol@aol.com>
2013-01-25 19:46:11 +0100

    Added objects to mainstate

--- GGJ2013/src/states/mainstate.lua
+++ GGJ2013/src/states/mainstate.lua
@@ -2,16 +2,27 @@
 
 require("core/gamestate")
 require("core/resources")
+require("core/objectgroup")
 
 MainState = class("MainState", GameState)
 
+function MainState:__init()
+    self.objects = ObjectGroup()
+end
+
 function MainState:draw()
+    self.objects:draw()
+
     love.graphics.setBackgroundColor(17, 17, 17)
     love.graphics.setColor(255, 255, 255)
 
     love.graphics.clear()
     love.graphics.setFont(resources.fonts.normal)
     love.graphics.print("This is the game. Fuck yeah.", 10, 10)
+end
+
+function MainState:update(dt)
+    self.objects:update(dt)
 end
 
 function MainState:keypressed(k, u)
