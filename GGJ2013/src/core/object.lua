cced56021080b90e0092561ab2366f6db625e79d
Sven-Hendrik Haase <sh@lutzhaase.com>
2013-01-26 03:43:07 +0100

    Add toast, fix object deletion, add lifetime and suicide method

--- GGJ2013/src/core/object.lua
+++ GGJ2013/src/core/object.lua
@@ -7,12 +7,19 @@ function Object:__init()
     self.y = 0
     self.z = 0
     self.angle = 0
+    self.lifetime = 0
     self.group = nil
 end
 
-function Object:update(dt) end
+function Object:update(dt)
+    self.lifetime = self.lifetime + dt
+end
 
-function Object:draw()end
+function Object:suicide()
+    if self.group then self.group:remove(self) end
+end
+
+function Object:draw() end
 
 function Object:onAdd(group) end
 function Object:onRemove(group) end
