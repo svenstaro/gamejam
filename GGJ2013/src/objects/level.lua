5b0eebc778a1264adfe01cb0bae9a642660ad6a9
Fabian Stegemann <fabian.stegemann@gmx.net>
2013-01-26 01:14:01 +0100

    parsing all tilesets, layers

--- GGJ2013/src/objects/level.lua
+++ GGJ2013/src/objects/level.lua
@@ -14,25 +14,29 @@ function Level:__init()
     self.spritebatch = love.graphics.newSpriteBatch(resources.images.level, level.width * level.height)
     self.quads = {}
 
-    local spacing_top = 0
-    local spacing_left = 0
-    local tileset_tilewidth = ((level.tilesets[1].imageheight - (level.tilesets[1].imageheight % level.tileheight)) / level.tileheight)
-    local tileset_tileheight = ((level.tilesets[1].imagewidth - (level.tilesets[1].imagewidth % level.tilewidth)) / level.tilewidth)
-    for i=0, tileset_tileheight - 1 do
-        for j=0, tileset_tilewidth - 1 do
-            if j ~= 0 then spacing_left = level.tilesets[1].spacing else spacing_left = 0 end
-            if i ~= 0 then spacing_top = level.tilesets[1].spacing else spacing_top = 0 end
-            self.quads[level.tilesets[1].firstgid + j + (i * tileset_tileheight)] = love.graphics.newQuad(j * (level.tilewidth + spacing_left), i * (level.tileheight + spacing_top),
-                                                    level.tilewidth, level.tileheight,
-                                                    resources.images.level:getWidth(), resources.images.level:getHeight())
+    for t=1, #level.tilesets do
+        local spacing_top = 0
+        local spacing_left = 0
+        local tileset_tilewidth = ((level.tilesets[t].imageheight - (level.tilesets[t].imageheight % level.tileheight)) / level.tileheight)
+        local tileset_tileheight = ((level.tilesets[t].imagewidth - (level.tilesets[t].imagewidth % level.tilewidth)) / level.tilewidth)
+        for i=0, tileset_tileheight - 1 do
+            for j=0, tileset_tilewidth - 1 do
+                if j ~= 0 then spacing_left = level.tilesets[t].spacing else spacing_left = 0 end
+                if i ~= 0 then spacing_top = level.tilesets[t].spacing else spacing_top = 0 end
+                self.quads[level.tilesets[t].firstgid + j + (i * tileset_tileheight)] = love.graphics.newQuad(j * (level.tilewidth + spacing_left), i * (level.tileheight + spacing_top),
+                                                        level.tilewidth, level.tileheight,
+                                                        resources.images.level:getWidth(), resources.images.level:getHeight())
+            end
         end
     end
 
-    for i=0,level.height - 1 do
-        for j=0,level.width - 1 do
-            --print(level.layers[1].data[1 + j + (i * level.width)])
-            if level.layers[1].data[1 + j + (i * level.width)] ~= 0 then
-                self.spritebatch:addq(self.quads[level.layers[1].data[1 + j + (i * level.width)]], j * level.tilewidth, i * level.tileheight)
+    for l=1, #level.layers do
+        for i=0, level.height - 1 do
+            for j=0,level.width - 1 do
+                --print(level.layers[1].data[1 + j + (i * level.width)])
+                if level.layers[l].data[1 + j + (i * level.width)] ~= 0 then
+                    self.spritebatch:addq(self.quads[level.layers[l].data[1 + j + (i * level.width)]], j * level.tilewidth, i * level.tileheight)
+                end
             end
         end
     end
