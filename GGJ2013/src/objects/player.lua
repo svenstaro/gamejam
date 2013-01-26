1958969b374771c60c6dc148f536bba1cecf76d3
Paul Bienkowski <opatutlol@aol.com>
2013-01-26 20:02:46 +0100

    Real time real shadow, fake mode

--- GGJ2013/src/objects/player.lua
+++ GGJ2013/src/objects/player.lua
@@ -20,6 +20,45 @@ function Player:__init()
     self.anim = newAnimation(resources.images.player_anim, 16, 16, 0.1, 4)
 
     self.physicsObject = {}
+
+    self.shadowMapData = love.image.newImageData(512, 1)
+    self.shadowMap = nil
+end
+
+function Player:bakeShadows(world)
+    local width = self.shadowMapData:getWidth()
+    for i = 0, width  - 1 do
+        local alpha = i * math.pi * 2 / width
+        local length = 1000
+        local dx, dy = self.x + math.cos(alpha) * length, self.y - math.sin(alpha) * length
+
+        local results = {}
+        world:rayCast(self.x, self.y, dx, dy, function(fixture, x, y, xn, yn, fraction)
+            local user = fixture:getUserData()
+            if user.__name == "Player" then return 1 end
+
+            --love.graphics.setColor(255, 0, 0)
+            --love.graphics.circle("fill", x, y, 2)
+
+            local d = math.sqrt((self.x - x) * (self.x - x) + (self.y - y) * (self.y - y))
+            table.insert(results, d)
+            return 1
+        end)
+
+        local smallest = 1000
+        for k, v in pairs(results) do
+            if smallest > v then smallest = v end
+        end
+        local d = smallest * 1000
+
+        local r = d % 256
+        local g = ((d - r) / 256) % 256
+        local b = (d - g * 256) / 256 / 256
+
+        self.shadowMapData:setPixel(i, 0, r, g, b, 255)
+    end
+    self.shadowMap = love.graphics.newImage(self.shadowMapData)
+    --self.shadowMap:setFilter("nearest", "nearest")
 end
 
 function Player:enablePhysics()
@@ -41,8 +80,8 @@ function Player:update(dt)
 
     local mx, my = main:getMousePosition()
     local tmphead_angle = math.atan2(my - self.y, mx - self.x)
-    
-    if math.abs(math.abs(tmphead_angle) - math.abs(self.walk_angle)) < 1 then 
+
+    if math.abs(math.abs(tmphead_angle) - math.abs(self.walk_angle)) < 1 then
         self.head_angle = math.atan2(my - self.y, mx - self.x)
     end
 
@@ -122,11 +161,14 @@ function Player:update(dt)
 
     love.audio.setPosition(self.x, 0, self.y)
     love.audio.setOrientation(math.cos(self.walk_angle), math.sin(self.walk_angle), 0, 0, 0, -1, 0)
-    
+
 end
 
 function Player:draw()
     love.graphics.setColor(255, 255, 255)
     self.anim:draw(self.x, self.y, self.walk_angle, 4, 4, 8, 8)
     love.graphics.draw(self.head, self.x, self.y, self.head_angle, 1, 1, 32, 32)
+
+    -- debug
+    -- if self.shadowMap then love.graphics.draw(self.shadowMap, self.x, self.y - 20, 0, 1, 10) end
 end
