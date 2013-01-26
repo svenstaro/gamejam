edef31fb1eaf0d4a86dbfa6fe9a6739c9880b23b
Rafael Epplee <rafael@fwd.io>
2013-01-26 06:16:54 +0100

    added new player movement graphics

--- GGJ2013/src/objects/player.lua
+++ GGJ2013/src/objects/player.lua
@@ -10,20 +10,24 @@ function Player:__init()
     self.x = 0
     self.y = 0
     self.z = 1
-    self.angle = 0
+    self.walk_angle = 0
+    self.head_angle = 0
     self.timeSinceLastStep = 0
     self.anim = newAnimation(resources.images.player_anim, 16, 16, 0.1, 4)
+    self.head = resources.images.player_head
 end
 
 function Player:update(dt)
     local mx, my = main:getMousePosition()
-    self.angle = math.atan2(my - self.y, mx - self.x)
+    self.head_angle = math.atan2(my - self.y, mx - self.x)
 
     local dx, dy = 0, 0
     if love.keyboard.isDown("a") then dx = dx - 1 end
     if love.keyboard.isDown("d") then dx = dx + 1 end
     if love.keyboard.isDown("w") then dy = dy - 1 end
     if love.keyboard.isDown("s") then dy = dy + 1 end
+    if math.abs(dy) + math.abs(dx) > 0 then self.walk_angle = math.atan2(dy, dx) end
+
     if math.abs(dy) + math.abs(dx) == 2 then
         dx = dx * 0.84
         dy = dy * 0.84
@@ -47,12 +51,13 @@ function Player:update(dt)
     self.anim:update(dt)
 
     love.audio.setPosition(self.x, 0, self.y)
-    love.audio.setOrientation(math.cos(self.angle), math.sin(self.angle), 0, 0, 0, -1, 0)
+    love.audio.setOrientation(math.cos(self.walk_angle), math.sin(self.walk_angle), 0, 0, 0, -1, 0)
 
 end
 
 function Player:draw()
     love.graphics.setColor(255, 255, 255)
-    self.anim:draw(self.x, self.y, self.angle, 4, 4, 8, 8)
+    self.anim:draw(self.x, self.y, self.walk_angle, 4, 4, 8, 8)
+    love.graphics.draw(self.head, self.x, self.y, self.head_angle, 1, 1, 32, 32)
 end
  
