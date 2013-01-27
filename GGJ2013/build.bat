fa02718fb62cc45a3042ffa3b7ecfcaf6372b8a5
spa14ce <spaaaaaaaaaaaaaace@gmail.com>
2013-01-28 00:45:52 +0100

    changed path to existing folder (windows32 -> windows_x86)

--- GGJ2013/build.bat
+++ GGJ2013/build.bat
@@ -3,7 +3,7 @@ cd src\
 ..\lib\7z.exe a -r -tzip ..\game.love *
 cd ..
 if %PROCESSOR_ARCHITECTURE%==x86 (
-  lib\windows32\love.exe game.love
+  lib\windows_x86\love.exe game.love
 ) else (
-  lib\windows64\love.exe game.love --console
+  lib\windows_x64\love.exe game.love --console
 )
