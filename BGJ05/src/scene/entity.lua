ab93213386ee0b6bf911b6e6ca8b7eba52a6425c
Sven-Hendrik Haase <sh@lutzhaase.com>
2013-06-08 16:08:50 +0200

    Awesome LampChain

--- BGJ05/src/scene/entity.lua
+++ BGJ05/src/scene/entity.lua
@@ -17,7 +17,7 @@ function Entity:update(dt)
     self.lifetime = self.lifetime + dt
 
     if self.physicsObject ~= nil and self.physicsObject.body ~= nil then
-        self.position = Vector(self.physicsObject.body:getX(), self.physicsObject.body:getY()) + (self.velocity * dt)
+        self.position = Vector(self.physicsObject.body:getX(), self.physicsObject.body:getY()) + (self.velocity * dt) -- < TODO wut
         self.physicsObject.body:setPosition(self.position.x, self.position.y)
         self.physicsObject.body:setAngle(self.rotation)
     else
