92f94b1169d6262e4775d11c7558667fbd838b42
Sven-Hendrik Haase <sh@lutzhaase.com>
2013-06-08 23:58:50 +0200

    Airplanes and antennas and shit

--- BGJ05/src/entities/lamp-antenna.lua
+++ BGJ05/src/entities/lamp-antenna.lua
@@ -19,10 +19,14 @@ function LampAntenna:onDraw()
     Lamp.onDraw(self)
 
     love.graphics.setColor(0, 0, 0)
-    love.graphics.rectangle("fill", self.position.x - self.size.x/2, self.position.y,
+    love.graphics.rectangle("fill", self.position.x - self.size.x/2, self.position.y - self.size.y,
                             self.size.x, self.size.y)
 
     love.graphics.setColor(unpack(self.glowColor))
-    love.graphics.rectangle("fill", self.position.x - self.size.x/2, self.position.y,
+    love.graphics.rectangle("fill", self.position.x - self.size.x/2, self.position.y - self.size.y,
                             self.size.x, 20)
+end
+
+function LampAntenna:getPosition()
+    return Vector(self.position.x, self.position.y - self.size.y + 10)
 end
