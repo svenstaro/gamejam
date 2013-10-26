0c3fcbee6bbfc0224c7ec26d8f16b0cdd52c2407
Alisa Dammer <alisa.dammer@gmail.com>
2013-10-26 14:55:12 +0200

    Forgot building.js

---
+++ BGJ06/src/building.js
@@ -0,0 +1,22 @@
+var Building = Class.create(Entity, {
+    initialize: function(posX, posY) {
+        this.height = THREE.Math.randInt(1,8);
+        //shape
+        this.geometry = new THREE.CubeGeometry(1, 1, this.height);
+        //color
+        this.material = new THREE.MeshLambertMaterial(0x999966);
+        //this.mesh = new THREE.Mesh(this.geometry, new THREE.MeshLambertMaterial( { envMap: THREE.ImageUtils.loadTexture( 'textures/metal.jpg', new THREE.SphericalReflectionMapping() ), overdraw: true } ));
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.posX = posX;
+        this.posY = posY;
+    },
+
+    onAdd: function(scene) {
+        scene.add(this.mesh);
+    },
+
+    update: function(dt) {
+        //this.sphere.rotation.x += 1 * delta;
+        //this.sphere.rotation.y += 1 * delta;
+    }
+});
