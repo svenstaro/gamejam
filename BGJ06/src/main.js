fa40091d633c55f903a9cccbc0ba668a39620de8
Alisa Dammer <alisa.dammer@gmail.com>
2013-10-26 14:51:19 +0200

    buildings

--- BGJ06/src/main.js
+++ BGJ06/src/main.js
@@ -1,5 +1,7 @@
 var App = new Game();
 var clock = new THREE.Clock(true);
+var building = new Building(THREE.Math.randInt(0, 100), THREE.Math.randInt(0, 100));
+App.addEntity(building);
 function mainloop () {
     requestAnimationFrame(mainloop);
     App.update(clock.getDelta());
