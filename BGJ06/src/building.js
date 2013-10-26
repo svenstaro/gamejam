bf45ca32681ede49188bf312d4207f794bde5bcd
Sven-Hendrik Haase <sh@lutzhaase.com>
2013-10-26 18:16:46 +0200

    Experimental shadow maps

--- BGJ06/src/building.js
+++ BGJ06/src/building.js
@@ -10,6 +10,8 @@ var Building = Class.create(Entity, {
         this.mesh.position.x = posX;
         this.mesh.position.y += this.height / 2
         this.mesh.position.z = posZ;
+        this.mesh.castShadow = true;
+        this.mesh.receiveShadow = true;
     },
 
     onAdd: function(scene) {
