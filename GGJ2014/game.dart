9090205935a25646a5c256254edaa1561f2ce820
Sascha Graeff <sascha@blackicegames.de>
2014-01-24 21:28:40 +0100

    Added clock and example behaviour code stuff.

--- GGJ2014/game.dart
+++ GGJ2014/game.dart
@@ -1,6 +1,7 @@
 import 'dart:html' as html;
 import 'package:stagexl/stagexl.dart';
 import 'global.dart';
+import 'clock.dart';
 
 void main() {
     // setup the Stage and RenderLoop
@@ -8,10 +9,21 @@ void main() {
     stage = new Stage('myStage', canvas);
     var renderLoop = new RenderLoop();
     renderLoop.addStage(stage);
+    
+    stage.addChild(new Clock());
 
     // draw a red circle
+    var parent = new Sprite();
     var shape = new Shape();
     shape.graphics.circle(100, 100, 60);
     shape.graphics.fillColor(Color.Red);
-    stage.addChild(shape);
+
+    
+    parent.onEnterFrame.listen((EnterFrameEvent e){
+        //move faster the later the current daytime is
+        parent.transformationMatrix.translate(Clock.daytime * e.passedTime * 20, 0);
+    });
+
+    shape.addTo(parent);
+    stage.addChild(parent);
 }
