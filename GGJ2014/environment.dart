24bccf65a969ea62177834d1b5f645755ab35d64
Sascha Graeff <sascha@blackicegames.de>
2014-01-25 16:51:48 +0100

    Wind and Rain. WORKS. 3 HOURS OF WORK. ONE BUG. SHIEEET

--- GGJ2014/environment.dart
+++ GGJ2014/environment.dart
@@ -15,16 +15,17 @@ class Environment extends Animatable {
     
     Environment() {
         _rainGen = makeOctave2(simplex2, 3, 0.01);
-        _windGen = makeOctave2(simplex2, 3, 0.01);
+        _windGen = makeOctave2(simplex2, 4, 0.4);
     }
     
     bool advanceTime(num time) {
         _time += time;
         
         
-        //Wind.windPower = (_windGen(_seed, time * 0.00002) * -2);
-        Wind.secondsPerWave = _windGen(_seed, time * 0.2) * 220;
-        //print(_windGen(_seed, time * 0.00002));
+        var wp = _windGen(_seed, _time * 0.002);
+        Wind.windPower = (wp * .2);
+        Wind.secondsPerWave = ((wp + 1) / 2) * 4 + 1;
+        //print("${Wind.secondsPerWave} und ${Wind.windPower}");
         
         amountOfRain = (_rainGen(_seed, _time * 0.01)).clamp(0,1) * 100;
     }
